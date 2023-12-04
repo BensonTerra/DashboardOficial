5fef7c41841b5dd7975d8f5e383ecd3797df0f45
BensonTerra <benson.terra@hotmail.com>
2023-12-04 11:38:20 +0000

    oraganização

--- src/components/map.vue
+++ src/components/map.vue
@@ -18,58 +18,54 @@
 </template>
  
  <script>
- import { LMap, LTileLayer,LMarker } from "@vue-leaflet/vue-leaflet";
- import { getDynamicMapHeight } from "../App.vue"
+  import { LMap, LTileLayer,LMarker } from "@vue-leaflet/vue-leaflet";
+  import { getDynamicMapHeight } from "../App.vue"
 
  
- export default {
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker
-   },
-  data () {
-    return {
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      center: [51.505, -0.04],
-      zoom: 13,
-      maxBounds: [ [-90, -180], [90, 180] ],
-      mapHeight: 0,
-      markPoint: [51.505, -0.04],
-    }
-  },
-  methods: {
-    zoomUpdated (zoom) {
-      this.zoom = zoom;
+  export default 
+  {
+    components: {
+      LMap,
+      LTileLayer,
+      LMarker
     },
-    centerUpdated (center) {
-      this.center = center;
-    },
-    heightUpdated() {
-      this.mapHeight = parseInt(getDynamicMapHeight(this.$root),10); //console.log(typeof(this.mapHeight))
-
-      var tileSize = 256; // tamanho padrão do tile no Leaflet
-      var minZoom = 0;
-      while ((tileSize * Math.pow(2, minZoom)) < this.mapHeight) 
-      {
-        //console.log(minZoom)
-        minZoom++;
+    data () {
+      return {
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        center: [51.505, -0.04],
+        zoom: 13,
+        maxBounds: [ [-90, -180], [90, 180] ],
+        mapHeight: 0,
+        markPoint: [51.505, -0.04],
       }
-      this.mapHeight = minZoom; //console.log(this.mapHeight)
-    }
+    },
+    methods: {
+      zoomUpdated (zoom) {
+        this.zoom = zoom;
+      },
+      centerUpdated (center) {
+        this.center = center;
+      },
+      heightUpdated() {
+        this.mapHeight = parseInt(getDynamicMapHeight(this.$root),10); //console.log(typeof(this.mapHeight))
 
-
-  },
-  created () {
-    setTimeout(() => 
-    {
-      this.mapHeight = parseInt(getDynamicMapHeight(this.$root),10); //console.log(this.mapHeight); 
-    }, 10);
-  },
-
-
-
- }
+        var tileSize = 256; // tamanho padrão do tile no Leaflet
+        var minZoom = 0;
+        while ((tileSize * Math.pow(2, minZoom)) < this.mapHeight) 
+        {
+          //console.log(minZoom)
+          minZoom++;
+        }
+        this.mapHeight = minZoom; //console.log(this.mapHeight)
+      }
+    },
+    created () {
+      setTimeout(() => 
+      {
+        this.mapHeight = parseInt(getDynamicMapHeight(this.$root),10); //console.log(this.mapHeight); 
+      }, 10);
+    },
+  }
  </script> 
  
  <style scoped>
